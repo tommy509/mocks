29584716ef37ec4e035fce9e30545ea64a0415b5
onofrejc <onofrejc@globalhitss.com>
2021-05-05 09:55:12 -0500

    Making  refactoring

---
+++ data/customer.js
@@ -0,0 +1,12 @@
+let customerData = [
+    {
+        id: 1,
+        name: "Customer 1"
+    },
+    {
+        id: 2,
+        name: "Customer 2"
+    },
+];
+
+module.exports = customerData;
--- typeDefs/schema.js
+++ typeDefs/schema.js
@@ -7,9 +7,7 @@ module.exports = buildSchema(`
         simList: [ simDetails ],
         simDetails(imsi: ID!): simDetails,
         simLastSessionDetails(imsi: Float!): simLastSessionDetails,
-        simChangeStatus(simChangeId: String!): simChangeStatus,
-        simFinishSleep(imsi: ID!, serviceProfileId: String): simFinishSleep,
-        simFinishTests(imsi: ID!, serviceProfileId: String, state: String): simFinishTests
+        simChangeStatus(simChangeId: String!): simChangeStatus
     }
 
     type simChangeStatus{
@@ -30,7 +28,7 @@ module.exports = buildSchema(`
         imei: String,
         labels: [label],
         status: String,
-        customer: Persona,
+        customer: Customer,
         businessUnit: String
     }
 
@@ -51,35 +49,18 @@ module.exports = buildSchema(`
         name: String
     }
 
-    type Persona{
+    type Customer{
         id: ID!,
         name: String
     }
 
     type lastSessionLocation{
-        mcc: Int,
-        mnc: Int,
-        lac: Int,
-        cell: Int
+    id: ID!
+    mcc: Int,
+    mnc: Int,
+    lac: Int,
+    cell: Int
     }
 
-    type simFinishSleep{
-        id: String,
-        simId: String,
-        requestedTime: String,
-        completionTime: String,
-        changeType: [String],
-        creationTime: String,
-        state: String
-    }
-
-    type simFinishTests{
-        id: String,
-        simId: String,
-        requestedTime: String,
-        completionTime: String,
-        creationTime: String,
-        changeType: [String],
-        state: String
-    }
+    type Mutation {}
 `);
