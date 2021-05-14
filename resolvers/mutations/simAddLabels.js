d3c4026aa257cd4e1424ecb5520c1fcc01302072
tommy509 <claudewendell@yahoo.fr>
2021-05-14 12:37:06 -0500

    Add label update

--- resolvers/mutations/simAddLabels.js
+++ resolvers/mutations/simAddLabels.js
@@ -7,7 +7,11 @@ var resolvers = {
     simAddLabels: (args) => {
         console.log(args)
         const simDetail = models.simDetail.find(simDetail => simDetail.imsi === args.input.imsi);
-        simDetail.labels.push(args.input.label);
+        let labels = args.input.label;
+        for(var i = 0; i<labels.length;i++){
+            simDetail.labels.push(labels[i]);
+        }
+       
         console.log(args.input.labels)
         return simDetail;
     }
