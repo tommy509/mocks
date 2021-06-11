8e64e638e3c19b44f71d0a9df64d10e0918d0847
Nitai Charan <nitaicharan@gmail.com>
2021-06-11 13:55:14 -0300

    Added enum type ISOCountry

--- resolvers/enums/index.js
+++ resolvers/enums/index.js
@@ -1,11 +1,13 @@
 const simChangeType = require('./simChangeType');
 const testsCompletedSimState = require('./testsCompletedSimState');
-const simRestriction = require('./simRestriction')
+const simRestriction = require('./simRestriction');
+const ISOCountry = require('./ISOCountry');
 
 const models = {
     simChangeType,
     testsCompletedSimState,
     simRestriction,
+    ISOCountry,
 };
 
 module.exports = models;
