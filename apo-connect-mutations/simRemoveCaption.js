761dfa7703d73770b519c2dc517e8f9b51d30d39
tommy509 <claudewendell@yahoo.fr>
2021-06-15 16:57:43 -0500

    simRemoveCaption

---
+++ apo-connect-mutations/simRemoveCaption.js
@@ -0,0 +1,40 @@
+var axios = require('axios');
+const https = require('https');
+const configuration = require("../config");
+configuration.getToken()
+  .then(token => {
+    //QUERY OR MUTATION
+    var data = JSON.stringify({
+        query: `mutation {
+        simRemoveCaption(imsi: 334020226183074){
+          imsi
+          caption
+        }
+    }
+      `,
+        variables: {}
+      });
+    //ACCESS TO API-CLAROCONNECT
+    
+// At request level
+const agent = new https.Agent({  
+  rejectUnauthorized: false
+});
+    let config = {
+      method: 'post',
+      url:configuration.endpoint,
+      httpsAgent: agent ,
+      headers: { 
+        'Authorization':'Bearer '+token, 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }).catch(err => console.log(err));
