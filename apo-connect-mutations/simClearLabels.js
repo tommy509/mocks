60abe2cbeb9db314f30462c30783591bd4575123
tommy509 <claudewendell@yahoo.fr>
2021-06-15 16:37:16 -0500

    simAddlabels-simClearLabels-simRemoveLabels

---
+++ apo-connect-mutations/simClearLabels.js
@@ -0,0 +1,39 @@
+var axios = require('axios');
+const https = require('https');
+const configuration = require("../config");
+configuration.getToken()
+  .then(token => {
+    //QUERY OR MUTATION
+    var data = JSON.stringify({
+        query: `mutation {
+        simClearLabels(imsi:334020226183074) {
+          imsi
+          labels
+        }
+      }`,
+        variables: {}
+      });
+    //ACCESS TO API-CLAROCONNECT
+    
+// At request level
+const agent = new https.Agent({  
+  rejectUnauthorized: false
+});
+    let config = {
+      method: 'post',
+      url:configuration.endpoint,
+      httpsAgent: agent ,
+      headers: { 
+        'Authorization':'Bearer '+token, 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }).catch(err => console.log(err));
