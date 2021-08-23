ad0d70bddf3a47990a41da2169b2a7720e25d1c0
Nitai Charan <nitaicharan@gmail.com>
2021-08-23 14:29:46 -0300

    Finished login endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const urlModule = require('url');
 const axios = require('axios')
 const oauth = require('axios-oauth-client');
 var { graphqlHTTP } = require('express-graphql');
@@ -8,51 +9,24 @@ var schema = require('./typeDefs');
 var resolvers = require('./resolvers');
 
 var app = express();
-app.use('/graphql', authCodeGrant, graphqlHTTP({
+
+app.get('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
   graphiql: true,
 }));
 
+app.post('/login', (req, res) => {
+  const url = new urlModule.URL('http://localhost:3333/code-grant');
+  url.searchParams.append('response_type', 'code');
+  url.searchParams.append('client_id', '8b29078c-370b-4e1d-9efd-ab95d2ab7bde');
+  url.searchParams.append('redirect_uri', 'http://localhost:5000/checkCode');
+  url.searchParams.append('state', 'REQUIRED');
 
-
-app.get('/checkCode', async (req, res) => {
-  const { code, state } = req.query;
-
-  //Send to Auth Server to validate and Receive Access token
-  const result = await axios.post('http://localhost:3333/access_token', {
-    json: true,
-    body: {
-      client_id: '',
-      client_secret: '',
-      code,
-      state,
-    },
-  });
-
-  res.redirect()
-
+  res.redirect(307, url);
 })
 
-async function authCodeGrant(req, res, next) {
-
-  console.log("Redirecionar para AuthServer");
-  // 1 - Acquire the Authorization Code by accessing code-grant
-  const getAuthorizationCode = oauth.client(axios.create(), {
-    url: 'http://localhost:3333/code-grant',
-    response_type: 'code',
-    client_id: '8b29078c-370b-4e1d-9efd-ab95d2ab7bde',
-    redirect_uri: 'http://localhost:5000/checkCode',
-    state: 'REQUIRED'
-  });
-
-  const auth = await getAuthorizationCode();
-
-  console.log(auth)
-
-  res.redirect_uri('/checkCode', auth);
-}
-
-app.listen(5000, () => {
-  console.log('Running a GraphQL API server at localhost:5000/graphql');
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Running a GraphQL API server at localhost:${PORT}/graphql`);
 });
