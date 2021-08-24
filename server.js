1ca0cbcc9645269ef10fde6089b432b8072a287b
Nitai Charan <nitaicharan@gmail.com>
2021-08-24 13:14:27 -0300

    Fixed redirect address

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 const urlModule = require('url');
 const axios = require('axios')
-const oauth = require('axios-oauth-client');
 var { graphqlHTTP } = require('express-graphql');
 
 // Construct a schema, using GraphQL schema language
@@ -17,21 +16,17 @@ app.get('/graphql', graphqlHTTP({
 }));
 
 app.post('/login', (req, res) => {
-  const redirectUrl = new URL('http://google.com');
-
-  const url = new urlModule.URL('http://localhost:5000/checkCode');
+  const url = new urlModule.URL('http://localhost:3333/code-grant');
   url.searchParams.append('response_type', 'code');
   url.searchParams.append('client_id', '8b29078c-370b-4e1d-9efd-ab95d2ab7bde');
-  url.searchParams.append('redirect_uri', url);
+  url.searchParams.append('redirect_uri', "http://localhost:5000/checkCode");
   url.searchParams.append('state', 'REQUIRED');
 
   res.redirect(307, url);
 })
 
 app.post('/checkCode', (req, res) => {
-  
-  console.log("AAccessing endpoint CheckCode")
-
+  res.send(req.query);
 })
 
 const PORT = 5000;
