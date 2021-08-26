ad59e1a29273fc681db430f25872c943c33b02d1
JuarezCout <juarezcout15.jl@gmail.com>
2021-08-26 13:00:47 -0300

    Added Code Grant flow

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var resolvers = require('./resolvers');
 
 var app = express();
 
-app.get('/graphql', graphqlHTTP({
+app.use('/graphql', verifyToken, graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
   graphiql: true,
@@ -17,19 +17,43 @@ app.get('/graphql', graphqlHTTP({
 
 app.post('/login', (req, res) => {
   const url = new urlModule.URL('http://localhost:3333/code-grant');
+  const redirectUrl = new urlModule.URL('http://localhost:5000/checkCode');
+
   url.searchParams.append('response_type', 'code');
-  url.searchParams.append('client_id', '8b29078c-370b-4e1d-9efd-ab95d2ab7bde');
-  url.searchParams.append('redirect_uri', "http://localhost:5000/checkCode");
+  url.searchParams.append('client_id', '5a6268b6-635e-44e9-bf4b-55bc8b6bb2fa');
+  url.searchParams.append('redirect_uri', redirectUrl);
   url.searchParams.append('state', 'REQUIRED');
 
   res.redirect(307, url);
 })
 
 app.post('/checkCode', (req, res) => {
-  res.send(req.query);
+  const url = new urlModule.URL('http://localhost:3333/access-token');
+  const redirectUrl = new urlModule.URL('http://localhost:5000/getToken');
+
+  url.searchParams.append('grant_type', 'authorization_code');
+  url.searchParams.append('code', req.query.code);
+  url.searchParams.append('client_id', '5a6268b6-635e-44e9-bf4b-55bc8b6bb2fa');
+  url.searchParams.append('client_secret', '27fd83d7-9663-493b-8806-1e93ee16c484');
+  url.searchParams.append('redirect_uri', redirectUrl);
+
+  res.redirect(307, url);
+})
+
+app.post('/getToken', (req, res) => {
+  res.send({code: req.query.access_token})
 })
 
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Running a GraphQL API server at localhost:${PORT}/graphql`);
 });
+
+function verifyToken(req, res, next) {
+  console.log(req.query.access_token)
+  if(!req.query.access_token){
+    next();
+  } else {
+    res.send(401);
+  }
+}
