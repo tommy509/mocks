aa18a44d7695dc31f977f9ba8de67f28c1ac5960
JuarezCout <juarezcout15.jl@gmail.com>
2021-08-30 14:26:03 -0300

    Fixed Implicit Grant flow

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 const urlModule = require('url');
 const axios = require('axios')
 var { graphqlHTTP } = require('express-graphql');
+var opn = require('opn');
+
 
 // Construct a schema, using GraphQL schema language
 var schema = require('./typeDefs');
@@ -9,11 +11,13 @@ var resolvers = require('./resolvers');
 
 var app = express();
 
-app.use('/graphql', verifyToken, graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
   graphiql: true,
 }));
+
+// Code Grant Flow
 
 app.post('/loginCodeGrant', (req, res) => {
   const url = new urlModule.URL('http://localhost:3333/code-grant');
@@ -27,17 +31,6 @@ app.post('/loginCodeGrant', (req, res) => {
   res.redirect(307, url);
 })
 
-app.post('/loginImplicitGrant', (req, res) => {
-  const url = new urlModule.URL('http://localhost:3333/implicit-grant');
-  const redirectUrl = new urlModule.URL('http://localhost:5000/getToken');
-
-  url.searchParams.append('response_type', 'token');
-  url.searchParams.append('client_id', '5a6268b6-635e-44e9-bf4b-55bc8b6bb2fa');
-  url.searchParams.append('redirect_uri', redirectUrl);
-  url.searchParams.append('state', 'REQUIRED');
-
-  res.redirect(307, url);
-})
 
 app.post('/checkCode', (req, res) => {
   const url = new urlModule.URL('http://localhost:3333/access-token');
@@ -52,20 +45,44 @@ app.post('/checkCode', (req, res) => {
   res.redirect(307, url);
 })
 
+// Implicit Grant Flow
+
+app.post('/loginImplicitGrant', (req, res) => {
+  const url = new urlModule.URL('http://localhost:3333/implicit-grant');
+  const redirectUrl = new urlModule.URL('http://localhost:5000/checkToken');
+
+  url.searchParams.append('response_type', 'token');
+  url.searchParams.append('client_id', '5a6268b6-635e-44e9-bf4b-55bc8b6bb2fa');
+  url.searchParams.append('redirect_uri', redirectUrl);
+  url.searchParams.append('state', 'REQUIRED');
+
+  res.redirect(307, url);
+})
+
+
+app.post('/checkToken', (req, res) => {
+  const config = {
+    headers: { Authorization: `Bearer ${req.query.access_token}` }
+  };
+
+  const bodyParameters = {
+    key: "value"
+  };
+
+  axios.post(
+    'http://localhost:3333/check-token',
+    bodyParameters,
+    config
+  ).then(opn('http://localhost:5000/graphql')).catch(console.log);
+});
+
 app.post('/getToken', (req, res) => {
-  res.send({code: req.query})
+  if (req.query.access_token) {
+    opn('http://localhost:5000/graphql');
+  }
 })
 
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Running a GraphQL API server at localhost:${PORT}/graphql`);
 });
-
-function verifyToken(req, res, next) {
-  console.log(req.query.access_token)
-  if(!req.query.access_token){
-    next();
-  } else {
-    res.send(401);
-  }
-}
